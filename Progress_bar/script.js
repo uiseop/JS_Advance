2302f8a0e59e357f3c09a072191f417f4f1bc537
uiseop <bono521@naver.com>
2022-04-25 16:33:02 +0900

    reset 적용 및 디자인

---
+++ Progress_bar/script.js
@@ -0,0 +1,12 @@
+(function () {
+    const get = (target) => {
+        return document.querySelector(target);
+    };
+
+    const onscroll = () => {
+        
+    }
+
+    window.addEventListener("scroll", () => onscroll())
+
+})();

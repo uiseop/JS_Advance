ca90e406848ca549dbb70f85bb7b9f348b292790
uiseop <bono521@naver.com>
2022-04-26 17:29:49 +0900

    throttle 적용

--- InfiniteScroll/script.js
+++ InfiniteScroll/script.js
@@ -6,7 +6,8 @@
     const $items = get(".items");
     const API_URL = "https://jsonplaceholder.typicode.com/posts";
     const limit = 10;
-    const page = 1;
+    let page = 1;
+    let timerId;
 
     const getPost = async () => {
         try {
@@ -24,7 +25,7 @@
 
     const loadPost = async () => {
         const posts = await getPost();
-        console.log(posts);
+        // console.log(posts);
         for (let post of posts) {
             let html = `<li class="item">
                 <div class="header">
@@ -37,7 +38,27 @@
         }
     };
 
+    const isEndScroll = () => {
+        const {scrollTop, scrollHeight, clientHeight} = document.documentElement
+        console.log(scrollTop, scrollHeight, clientHeight)
+        if(scrollTop + clientHeight >= scrollHeight) {
+            console.log("wow you in last")
+            page += 1;
+            loadPost();
+        }
+    }
+
+    const throttle = (callback, time) => {
+        if (!timerId) {
+            timerId = setTimeout(() => {
+                timerId = null;
+                callback()
+            }, time);
+        }
+    }
+
     window.addEventListener("DOMContentLoaded", () => {
         loadPost();
+        window.addEventListener("scroll", () => throttle(isEndScroll, 100))
     });
 })();

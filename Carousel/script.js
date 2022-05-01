a02d584c91652384eadf278f9d5dad2392327b74
uiseop <bono521@naver.com>
2022-05-01 21:17:15 +0900

    현재 이미지 위치 확인

--- Carousel/script.js
+++ Carousel/script.js
@@ -5,12 +5,25 @@
 
     const setCarousel = () => {
         const carousel = get(".carousel");
+        const dots = get(".dots");
         const items = carousel.querySelectorAll(".item");
         const totalItems = items.length;
+
+        for (let dot=0; dot < totalItems; dot++) {
+            const dotElement = document.createElement("div")
+            dotElement.className = "dot";
+            if (dot === 0) {
+                dotElement.classList.add("active")
+            }
+            dots.appendChild(dotElement);
+        }
+        const dotItems = dots.querySelectorAll(".dot")
         let current = 0;
         items[current].classList.add("active")
         items[1].classList.add("next")
         items[totalItems - 1].classList.add("prev")
+        
+
 
         const prevButton = get(".carousel_button.prev");
         const nextButton = get(".carousel_button.next");
@@ -43,16 +56,20 @@
             console.log(`prev: ${prev}, current: ${current}, next: ${next}`);
             for (let i = 0; i < totalItems; i++) {
                 if (i === current) {
+                    dotItems[i].classList.add("active")
                     items[i].className = "item"
                     items[i].classList.add("active");
                 } else if(i === prev) {
                     items[i].className = "item"
                     items[i].classList.add("prev")
+                    dotItems[i].className = "dot"
                 } else if(i === next) {
                     items[i].className = "item"
                     items[i].classList.add("next")
+                    dotItems[i].className = "dot"
                 } else {
                     items[i].className = "item"
+                    dotItems[i].className = "dot"
                 }
             }
         };
